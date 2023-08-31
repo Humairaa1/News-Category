e63bccf4abda7fadaefc101bd58cae617c046506
Humairaa1 <humaira71bd@gmail.com>
2023-08-31 11:28:15 +0600

    News Catagory website stars

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const handleCategory =async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    console.log(data.data.news_category);
+}
+handleCategory();
